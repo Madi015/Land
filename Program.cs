7c711740961f013757849240ac8e8aa5ade2422f
Madi015 <madisaed2003@yahoo.com>
2023-02-23 12:03:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Land
+{
+    internal class Program
+    {
+        public class Land
+        {
+            public string namn, styrestyp, huvudstad, invånarantal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
